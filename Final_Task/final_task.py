b9886192a2f2430349dd7006f2fa53126d5762a3
zaminmammado.v <memmedovzamin646@gmail.com>
2026-06-16 22:19:41 +0000

    Complete final inverted index task

--- Final_Task/final_task.py
+++ Final_Task/final_task.py
@@ -1,101 +1,477 @@
-"""
-Module for preparing inverted indexes based on uploaded documents
-"""
 
+
+import json
+import re
 import sys
 from argparse import ArgumentParser, ArgumentTypeError, FileType
 from io import TextIOWrapper
-from typing import Dict, List
+from typing import Dict, Iterable, List, Sequence, TextIO
 
 DEFAULT_PATH_TO_STORE_INVERTED_INDEX = "inverted.index"
 
+# A standard English stop-word list. The task requires stop-word removal before
+# building the inverted index.
+STOP_WORDS = {
+    "a",
+    "about",
+    "above",
+    "across",
+    "after",
+    "afterwards",
+    "again",
+    "against",
+    "all",
+    "almost",
+    "alone",
+    "along",
+    "already",
+    "also",
+    "although",
+    "always",
+    "am",
+    "among",
+    "amongst",
+    "amoungst",
+    "amount",
+    "an",
+    "and",
+    "another",
+    "any",
+    "anyhow",
+    "anyone",
+    "anything",
+    "anyway",
+    "anywhere",
+    "are",
+    "around",
+    "as",
+    "at",
+    "back",
+    "be",
+    "became",
+    "because",
+    "become",
+    "becomes",
+    "becoming",
+    "been",
+    "before",
+    "beforehand",
+    "behind",
+    "being",
+    "below",
+    "beside",
+    "besides",
+    "between",
+    "beyond",
+    "bill",
+    "both",
+    "bottom",
+    "but",
+    "by",
+    "call",
+    "can",
+    "cannot",
+    "cant",
+    "co",
+    "computer",
+    "con",
+    "could",
+    "couldnt",
+    "cry",
+    "de",
+    "describe",
+    "detail",
+    "do",
+    "done",
+    "down",
+    "due",
+    "during",
+    "each",
+    "eg",
+    "eight",
+    "either",
+    "eleven",
+    "else",
+    "elsewhere",
+    "empty",
+    "enough",
+    "etc",
+    "even",
+    "ever",
+    "every",
+    "everyone",
+    "everything",
+    "everywhere",
+    "except",
+    "few",
+    "fifteen",
+    "fify",
+    "fill",
+    "find",
+    "fire",
+    "first",
+    "five",
+    "for",
+    "former",
+    "formerly",
+    "forty",
+    "found",
+    "four",
+    "from",
+    "front",
+    "full",
+    "further",
+    "get",
+    "give",
+    "go",
+    "had",
+    "has",
+    "hasnt",
+    "have",
+    "he",
+    "hence",
+    "her",
+    "here",
+    "hereafter",
+    "hereby",
+    "herein",
+    "hereupon",
+    "hers",
+    "herself",
+    "him",
+    "himself",
+    "his",
+    "how",
+    "however",
+    "hundred",
+    "i",
+    "ie",
+    "if",
+    "in",
+    "inc",
+    "indeed",
+    "interest",
+    "into",
+    "is",
+    "it",
+    "its",
+    "itself",
+    "keep",
+    "last",
+    "latter",
+    "latterly",
+    "least",
+    "less",
+    "ltd",
+    "made",
+    "many",
+    "may",
+    "me",
+    "meanwhile",
+    "might",
+    "mill",
+    "mine",
+    "more",
+    "moreover",
+    "most",
+    "mostly",
+    "move",
+    "much",
+    "must",
+    "my",
+    "myself",
+    "name",
+    "namely",
+    "neither",
+    "never",
+    "nevertheless",
+    "next",
+    "nine",
+    "no",
+    "nobody",
+    "none",
+    "noone",
+    "nor",
+    "not",
+    "nothing",
+    "now",
+    "nowhere",
+    "of",
+    "off",
+    "often",
+    "on",
+    "once",
+    "one",
+    "only",
+    "onto",
+    "or",
+    "other",
+    "others",
+    "otherwise",
+    "our",
+    "ours",
+    "ourselves",
+    "out",
+    "over",
+    "own",
+    "part",
+    "per",
+    "perhaps",
+    "please",
+    "put",
+    "rather",
+    "re",
+    "same",
+    "see",
+    "seem",
+    "seemed",
+    "seeming",
+    "seems",
+    "serious",
+    "several",
+    "she",
+    "should",
+    "show",
+    "side",
+    "since",
+    "sincere",
+    "six",
+    "sixty",
+    "so",
+    "some",
+    "somehow",
+    "someone",
+    "something",
+    "sometime",
+    "sometimes",
+    "somewhere",
+    "still",
+    "such",
+    "system",
+    "take",
+    "ten",
+    "than",
+    "that",
+    "the",
+    "their",
+    "them",
+    "themselves",
+    "then",
+    "thence",
+    "there",
+    "thereafter",
+    "thereby",
+    "therefore",
+    "therein",
+    "thereupon",
+    "these",
+    "they",
+    "thick",
+    "thin",
+    "third",
+    "this",
+    "those",
+    "though",
+    "three",
+    "through",
+    "throughout",
+    "thru",
+    "thus",
+    "to",
+    "together",
+    "too",
+    "top",
+    "toward",
+    "towards",
+    "twelve",
+    "twenty",
+    "two",
+    "un",
+    "under",
+    "until",
+    "up",
+    "upon",
+    "us",
+    "very",
+    "via",
+    "was",
+    "we",
+    "well",
+    "were",
+    "what",
+    "whatever",
+    "when",
+    "whence",
+    "whenever",
+    "where",
+    "whereafter",
+    "whereas",
+    "whereby",
+    "wherein",
+    "whereupon",
+    "wherever",
+    "whether",
+    "which",
+    "while",
+    "whither",
+    "who",
+    "whoever",
+    "whole",
+    "whom",
+    "whose",
+    "why",
+    "will",
+    "with",
+    "within",
+    "without",
+    "would",
+    "yet",
+    "you",
+    "your",
+    "yours",
+    "yourself",
+    "yourselves",
+}
+
 
 class EncodedFileType(FileType):
-    """File encoder"""
+    """FileType that opens '-' as encoded stdin/stdout."""
 
     def __call__(self, string):
-        # the special argument "-" means sys.std{in,out}
+        # The special argument "-" means sys.std{in,out}.
         if string == "-":
             if "r" in self._mode:
-                stdin = TextIOWrapper(sys.stdin.buffer, encoding=self._encoding)
-                return stdin
+                return TextIOWrapper(sys.stdin.buffer, encoding=self._encoding)
             if "w" in self._mode:
-                stdout = TextIOWrapper(sys.stdout.buffer, encoding=self._encoding)
-                return stdout
+                return TextIOWrapper(sys.stdout.buffer, encoding=self._encoding)
             msg = 'argument "-" with mode %r' % self._mode
             raise ValueError(msg)
 
-        # all other arguments are used as file names
         try:
             return open(string, self._mode, self._bufsize, self._encoding, self._errors)
         except OSError as exception:
             args = {"filename": string, "error": exception}
             message = "can't open '%(filename)s': %(error)s"
-            raise ArgumentTypeError(message % args)
+            raise ArgumentTypeError(message % args) from exception
 
     def print_encoder(self):
-        """printer of encoder"""
+        """Print current file encoding."""
         print(self._encoding)
 
 
+def tokenize(text: str) -> List[str]:
+    """Split text into lowercase non-stopword tokens."""
+    return [
+        word
+        for word in re.split(r"\W+", text.lower())
+        if word and word not in STOP_WORDS
+    ]
+
+
 class InvertedIndex:
-    """
-    This module is necessary to extract inverted indexes from documents.
-    """
+    """Inverted index: maps words to document ids where they occur."""
 
     def __init__(self, words_ids: Dict[str, List[int]]):
-        pass
+        self.words_ids = words_ids
 
-    def query(self, words: List[str]) -> List[int]:
-        """Return the list of relevant documents for the given query"""
-        pass
+    def query(self, words: Sequence[str]) -> List[int]:
+        """Return document ids that contain every word from the query."""
+        query_words = tokenize(" ".join(words))
+        if not query_words:
+            return []
+
+        # Deduplicate query words while keeping their order.
+        ordered_query_words = list(dict.fromkeys(query_words))
+
+        first_word = ordered_query_words[0]
+        if first_word not in self.words_ids:
+            return []
+
+        other_doc_sets = [
+            set(self.words_ids.get(word, [])) for word in ordered_query_words[1:]
+        ]
+
+        return [
+            doc_id
+            for doc_id in self.words_ids[first_word]
+            if all(doc_id in doc_ids for doc_ids in other_doc_sets)
+        ]
 
     def dump(self, filepath: str) -> None:
         """
-        Allow us to write inverted indexes documents to temporary directory or local storage
-        :param filepath: path to file with documents
+        Write the inverted index to local storage as JSON.
+
+        :param filepath: path to the file where the index should be stored
         :return: None
         """
-        pass
+        with open(filepath, "w", encoding="utf-8") as index_file:
+            json.dump(self.words_ids, index_file, ensure_ascii=False)
 
     @classmethod
     def load(cls, filepath: str):
         """
-        Allow us to upload inverted indexes from either temporary directory or local storage
-        :param filepath: path to file with documents
+        Load an inverted index from local storage.
+
+        :param filepath: path to a JSON index file
         :return: InvertedIndex
         """
-        pass
+        with open(filepath, "r", encoding="utf-8") as index_file:
+            words_ids = json.load(index_file)
+
+        return cls({word: [int(doc_id) for doc_id in doc_ids] for word, doc_ids in words_ids.items()})
 
 
 def load_documents(filepath: str) -> Dict[int, str]:
     """
-    Allow us to upload documents from either tempopary directory or local storage
+    Load documents from a tab-separated file.
+
+    Each line has format: <document_id>\t<text>.
     :param filepath: path to file with documents
     :return: Dict[int, str]
     """
-    pass
+    documents: Dict[int, str] = {}
+
+    with open(filepath, "r", encoding="utf-8") as documents_file:
+        for line in documents_file:
+            if not line.strip():
+                continue
+            doc_id, content = line.lower().split("\t", 1)
+            documents[int(doc_id)] = content
+
+    return documents
 
 
 def build_inverted_index(documents: Dict[int, str]) -> InvertedIndex:
     """
-    Builder of inverted indexes based on documents
-    :param documents: dict with documents
-    :return: InvertedIndex class
+    Build an inverted index from documents.
+
+    :param documents: mapping of document id to document text
+    :return: InvertedIndex instance
     """
-    pass
+    words_ids: Dict[str, List[int]] = {}
+
+    for doc_id, content in documents.items():
+        seen_words_in_doc = set()
+        for word in tokenize(content):
+            if word in seen_words_in_doc:
+                continue
+            words_ids.setdefault(word, []).append(doc_id)
+            seen_words_in_doc.add(word)
+
+    return InvertedIndex(words_ids)
 
 
 def callback_build(arguments) -> None:
-    """process build runner"""
+    """Run the build command."""
     return process_build(arguments.dataset, arguments.output)
 
 
 def process_build(dataset, output) -> None:
     """
-    Function is responsible for running of a pipeline to load documents,
-    build and save inverted index.
-    :param arguments: key/value pairs of arguments from 'build' subparser
+    Load documents, build an inverted index, and save it.
+
+    :param dataset: path to dataset file
+    :param output: path where the index should be saved
     :return: None
     """
     documents: Dict[int, str] = load_documents(dataset)
@@ -104,92 +480,111 @@ def process_build(dataset, output) -> None:
 
 
 def callback_query(arguments) -> None:
-    """ "callback query runner"""
-    process_query(arguments.query, arguments.index)
+    """Run the query command."""
+    queries = arguments.query
+    if queries is None:
+        queries = TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
+    process_query(queries, arguments.index)
 
 
-def process_query(queries, index) -> None:
+def _normalize_query(query) -> List[str]:
+    """Convert a query line/list into normalized query words."""
+    if isinstance(query, str):
+        query_text = query
+    else:
+        query_text = " ".join(query)
+    return tokenize(query_text)
+
+
+def process_query(queries: Iterable, index) -> None:
     """
-    Function is responsible for loading inverted indexes
-    and printing document indexes for key words from arguments.query
-    :param arguments: key/value pairs of arguments from 'query' subparser
+    Load an inverted index, process every query, and print doc ids.
+
+    :param queries: iterable of query strings or query word lists
+    :param index: path to saved inverted index
     :return: None
     """
     inverted_index = InvertedIndex.load(index)
-    for query in queries:
-        print(query[0])
-        if isinstance(query, str):
-            query = query.strip().split()
 
-        doc_indexes = ",".join(str(value) for value in inverted_index.query(query))
+    for query in queries:
+        query_words = _normalize_query(query)
+        doc_indexes = ",".join(str(value) for value in inverted_index.query(query_words))
         print(doc_indexes)
 
 
 def setup_subparsers(parser) -> None:
     """
-    Initial subparsers with arguments.
-    :param parser: Instance of ArgumentParser
+    Initialize subparsers with arguments.
+
+    :param parser: instance of ArgumentParser
+    :return: None
     """
     subparser = parser.add_subparsers(dest="command")
+
     build_parser = subparser.add_parser(
         "build",
-        help="this parser is need to load, build"
-        " and save inverted index bases on documents",
+        help="load documents, build an inverted index, and save it",
     )
     build_parser.add_argument(
         "-d",
         "--dataset",
         required=True,
-        help="You should specify path to file with documents. ",
+        help="path to file with documents",
     )
     build_parser.add_argument(
         "-o",
         "--output",
         default=DEFAULT_PATH_TO_STORE_INVERTED_INDEX,
-        help="You should specify path to save inverted index. "
-        "The default: %(default)s",
+        help="path to save inverted index. The default: %(default)s",
     )
     build_parser.set_defaults(callback=callback_build)
 
     query_parser = subparser.add_parser(
-        "query", help="This parser is need to load and apply inverted index"
+        "query", help="load an inverted index and apply queries"
     )
     query_parser.add_argument(
         "--index",
         default=DEFAULT_PATH_TO_STORE_INVERTED_INDEX,
-        help="specify the path where inverted indexes are. " "The default: %(default)s",
+        help="path where the inverted index is stored. The default: %(default)s",
     )
-    query_file_group = query_parser.add_mutually_exclusive_group(required=True)
+    query_file_group = query_parser.add_mutually_exclusive_group(required=False)
     query_file_group.add_argument(
         "-q",
         "--query",
         dest="query",
         action="append",
         nargs="+",
-        help="you can specify a sequence of queries to process them overall",
+        help="query words; can be passed more than once",
     )
     query_file_group.add_argument(
         "--query_from_file",
+        "--from_file",
         dest="query",
         type=EncodedFileType("r", encoding="utf-8"),
-        # default=TextIOWrapper(sys.stdin.buffer, encoding='utf-8'),
-        help="query file to get queries for inverted index",
+        default=None,
+        help="path to file with one query per line; defaults to stdin if omitted",
     )
     query_parser.set_defaults(callback=callback_query)
 
 
 def main():
-    """
-    Starter of the pipeline
-    """
+    """CLI entry point."""
     parser = ArgumentParser(
-        description="Inverted Index CLI is need to load, build,"
-        "process query inverted index"
+        description="Inverted Index CLI: build an index and process search queries"
     )
     setup_subparsers(parser)
     arguments = parser.parse_args()
+
+    if not hasattr(arguments, "callback"):
+        parser.error("please specify a command: build or query")
+
     arguments.callback(arguments)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
